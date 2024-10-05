5a8eb41edf3f4b2ff3d616baa06b63a8c45f8414
bwjson <bwjson27@gmail.com>
2024-10-06 00:09:50 +0500

    postgres connection

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import asyncio
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
 
+from src.database.config import create_db, drop_db
 from src.config import load_config
 
 logger = logging.getLogger(__name__)
 
+# сам бот и роутер
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+async def on_startup(bot):
+	run_param = False
+	if run_param:
+		await drop_db()
+
+	await create_db()
+
+async def on_shutdown(bot):
+    print('Бот лег')
+
 # Точка входа функции программы
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     # получение конфигов
-    config = load_config('.env')
+    # config = load_config('.env')
 
     # конфигурацмя всех логиров 
     logging.basicConfig(
@@ -21,11 +39,7 @@ async def main():
     
     logger.info("Bot is starting")
 
-    # сам бот и роутер
-    bot = Bot(config.tg_bot.token)
-    dp = Dispatcher()
-
-    await bot.delete_webhook(drop_pending_updates=True) # Удаление прошлых не обработанных событий
+    # await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot) # Пуск бота
 
 
