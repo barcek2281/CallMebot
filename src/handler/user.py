43b72fd636bf1d858b2d507129854ce4c5eee4d7
barcek2281 <sabdp17@gmail.com>
2024-10-06 00:33:32 +0500

    handler v0.0.2

--- src/handler/user.py
+++ src/handler/user.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 # дочерный роутер
@@ -12,3 +12,18 @@ async def process_start_command(message: Message):
     await message.answer(text="Саламалейкум")
 
 
+@router.message(Command("sex"))
+async def process_ping_admins(message: Message):
+    
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    
+    members_count = await message.chat.get_member_count()
+    await message.answer(text=f"Пользователь вызвал функцию: {message.from_user.first_name}\nВсего пользователей: {members_count}")
+
+    admins = await message.bot.get_chat_administrators(chat_id)
+    print(type(admins))
+    if admins:
+        await message.answer(' '.join([f'@{admin.user.username}' for admin in admins if admin.user.username]))
+    else:
+        await message.answer("Ничего не получилось")
